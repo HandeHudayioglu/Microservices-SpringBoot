7920e538d3ab24c0dbca79d7b9a733f9a198a1d8
(Handehdy) <[handehudayioglu@gmail.com]>
2022-10-01 21:36:57 +0300

    01.10 dayoff

--- auth-service/src/main/java/com/hande/manager/IUserManager.java
+++ auth-service/src/main/java/com/hande/manager/IUserManager.java
@@ -11,12 +11,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 //tüm feign işlemlerine benzersiz adlandırmalar(name) yapılmalıdır
 
              //userprofilecontroller ın url si
-@FeignClient(url = "http://localhost:9091/v1/api/user", name = "user-service-userprofile", decode404 = true)
+@FeignClient(url = "${raceapplication.url.user}v1/api/user", name = "user-service-userprofile", decode404 = true)
 public interface IUserManager {
 
     //userprofilecontrollerdaki newUserCreate metodu
     @PostMapping("/newcreateuser")
-    ResponseEntity<Boolean>newUserCreate(NewUserCreateDto dto);
+    ResponseEntity<Boolean>NewUserCreate(NewUserCreateDto dto);
 
 
 }
